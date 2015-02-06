f8271955f929662bfc47e6da7112e6e17a8714e6
sailendra.n.jena@gmail.com <sailendra.n.jena@gmail.com@376d9f85-88c3-4b5c-aff6-862f136f3076>
2015-02-06 11:59:49 +0000

    Added By Sailendra Narayan Jena

--- branches/src/com/AccessDatabase/Test/Test.java
+++ branches/src/com/AccessDatabase/Test/Test.java
@@ -1,19 +1,8 @@
 package com.AccessDatabase.Test;
 
-import com.myAssets.utils.GmailUtil;
-
-
 public class Test
 {
 	public static void main(String[] args)
 	{
-		String username = "sailendra.n.jena@gmail.com";
-		String password = "shreemaa131";
-		String reciepentMail = "sailendra.n.jena@gmail.com";
-		String subjectLine = "Java Mail";
-		String textContent = "Hi Sailendra, "+"\n\n"+"This mail is coming from Java Code. Please don't reply on this mail.."+"\n\n"+"Thanks & Regards"+"\n"+"Sailendra Narayan Jena"+"\n"+"Mob: +919731728407";
-		String folderName = "INBOX";
-		GmailUtil.openFolder(username, password, folderName);
-		GmailUtil.disconnectGmailAcount();
 	}
 }
