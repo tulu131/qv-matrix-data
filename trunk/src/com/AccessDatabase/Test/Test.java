f1c9b3a4c5e72a2f994cdb62ae7e2b996c0fb7e1
sailendra.n.jena@gmail.com <sailendra.n.jena@gmail.com@376d9f85-88c3-4b5c-aff6-862f136f3076>
2015-02-13 11:02:24 +0000

    Changes Done By Sailendra Narayan Jena

--- trunk/src/com/AccessDatabase/Test/Test.java
+++ trunk/src/com/AccessDatabase/Test/Test.java
@@ -1,36 +1,61 @@
 package com.AccessDatabase.Test;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Test
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
-		try
+//		try
+//		{
+//			String data = "Sailendra Narayan Jena"+"\n";
+//			
+//			File file = new File("D:\\testlog.log");
+//			
+//			if (!file.exists())
+//			{
+//				file.createNewFile();
+//			}
+//			
+//			FileWriter fileWriter = new FileWriter(file, true);
+//			BufferedWriter brWriter = new BufferedWriter(fileWriter);
+//			brWriter.write(data);
+//			brWriter.newLine();
+//			brWriter.close();
+//			
+//			System.out.println("File Written Successfully............");
+//		}
+//		catch(IOException ex)
+//		{
+//			ex.printStackTrace();
+//		}
+		
+		BufferedReader brReader = new BufferedReader(new FileReader("src/com/myAssets/resources/DumpSample.txt"));
+		File file = new File("D:\\testlog.log");
+		FileWriter writer = new FileWriter(file, true);
+		if (!file.exists())
 		{
-			String data = "Sailendra Narayan Jena"+"\n";
-			
-			File file = new File("D:\\testlog.log");
-			
-			if (!file.exists())
-			{
-				file.createNewFile();
-			}
-			
-			FileWriter fileWriter = new FileWriter(file, true);
-			BufferedWriter brWriter = new BufferedWriter(fileWriter);
-			brWriter.write(data);
+			file.createNewFile();
+		}
+		BufferedWriter brWriter = new BufferedWriter(writer);
+		
+		String line = null;
+		
+		while ((line = brReader.readLine()) != null)
+		{
+			System.out.println(line);
+			brWriter.write(line);
 			brWriter.newLine();
-			brWriter.close();
-			
-			System.out.println("File Written Successfully............");
 		}
-		catch(IOException ex)
-		{
-			ex.printStackTrace();
-		}
+		brReader.close();
+		brWriter.close();
+		
+		System.out.println("File written Successfully.........");
 	}
 }
