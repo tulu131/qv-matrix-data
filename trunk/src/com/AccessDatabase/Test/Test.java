fa8dcc497fc0aea27377464f56f96972fcb352d6
sailendra.n.jena@gmail.com <sailendra.n.jena@gmail.com@376d9f85-88c3-4b5c-aff6-862f136f3076>
2015-02-20 11:57:00 +0000

    Changes Done By Sailendra Narayan Jena

--- trunk/src/com/AccessDatabase/Test/Test.java
+++ trunk/src/com/AccessDatabase/Test/Test.java
@@ -1,72 +1,10 @@
 package com.AccessDatabase.Test;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 public class Test
 {
-	public static void main(String[] args) throws IOException
+	public static void main(String[] args) 
 	{
-//		try
-//		{
-//			String data = "Sailendra Narayan Jena"+"\n";
-//			
-//			File file = new File("D:\\testlog.log");
-//			
-//			if (!file.exists())
-//			{
-//				file.createNewFile();
-//			}
-//			
-//			FileWriter fileWriter = new FileWriter(file, true);
-//			BufferedWriter brWriter = new BufferedWriter(fileWriter);
-//			brWriter.write(data);
-//			brWriter.newLine();
-//			brWriter.close();
-//			
-//			System.out.println("File Written Successfully............");
-//		}
-//		catch(IOException ex)
-//		{
-//			ex.printStackTrace();
-//		}
 		
-//		BufferedReader brReader = new BufferedReader(new FileReader("src/com/myAssets/resources/DumpSample.txt"));
-//		File file = new File("D:\\testlog.log");
-//		FileWriter writer = new FileWriter(file, true);
-//		if (!file.exists())
-//		{
-//			file.createNewFile();
-//		}
-//		BufferedWriter brWriter = new BufferedWriter(writer);
-//		
-//		String line = null;
-//		
-//		while ((line = brReader.readLine()) != null)
-//		{
-//			System.out.println(line);
-//			brWriter.write(line);
-//			brWriter.newLine();
-//		}
-//		brReader.close();
-//		brWriter.close();
-//		
-//		System.out.println("File written Successfully.........");
-		
-		
-		
-		String month = new SimpleDateFormat("MMMM").format(new Date(Calendar.getInstance().getTimeInMillis()));
-		String year = new SimpleDateFormat("yyyy").format(new Date(Calendar.getInstance().getTimeInMillis()));
-		
-		System.out.println(month);
-		System.out.println(year);
 	}
 }
