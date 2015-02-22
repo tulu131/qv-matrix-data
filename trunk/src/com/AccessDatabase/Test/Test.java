c883bb1ae3cdea1eb0b674b5fbdde71e1a7d2f75
sailendra.n.jena@gmail.com <sailendra.n.jena@gmail.com@376d9f85-88c3-4b5c-aff6-862f136f3076>
2015-02-22 19:42:20 +0000

    Changes Done By Sailendra Narayan Jena

--- trunk/src/com/AccessDatabase/Test/Test.java
+++ trunk/src/com/AccessDatabase/Test/Test.java
@@ -1,10 +1,15 @@
 package com.AccessDatabase.Test;
 
+import com.myAssets.utils.GmailUtil;
 
 public class Test
 {
 	public static void main(String[] args) 
 	{
+		String username = "sailendra.n.jena@gmail.com";
+		String password = "shreemaa131";
+		String folderName = "INBOX";
 		
+		GmailUtil.readMail(username, password, folderName);
 	}
 }
