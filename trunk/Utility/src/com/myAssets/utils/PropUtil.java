5758919f162c903c66c8de129f2e5fb4bc481531
sailendra.n.jena@gmail.com <sailendra.n.jena@gmail.com@376d9f85-88c3-4b5c-aff6-862f136f3076>
2015-03-04 09:09:17 +0000

    Added file By Sailendra Narayan Jena

---
+++ trunk/Utility/src/com/myAssets/utils/PropUtil.java
@@ -0,0 +1,48 @@
+package com.myAssets.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author Sailendra.Jena
+ *
+ */
+public class PropUtil
+{
+	public static File file = null;
+	public static FileInputStream fileInput = null;
+	public static Properties props = null;
+
+
+	static
+	{
+		file = new File("src\\com\\myAssets\\resources\\config.properties");
+		try
+		{
+			fileInput = new FileInputStream(file);
+			props = new Properties();
+			props.load(fileInput);
+			System.out.println("Properties file Loaded Successfully........");
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			FileUtil.destroyFileResources(null, fileInput, null, null, null, null, null, null, null, null, null, null);
+		}
+
+	}
+	
+	public static String getValues(String key)
+	{
+		String value = props.getProperty(key);
+		
+		return value;
+	}
+}
