614b38857e38c812fca4db0badb9e1b6699a99b7
sailendra.n.jena@gmail.com <sailendra.n.jena@gmail.com@376d9f85-88c3-4b5c-aff6-862f136f3076>
2015-03-04 07:11:14 +0000

    Added By Sailendra Narayan Jena

---
+++ trunk/StingPractice/src/com/lara/StringPractice/Test1.java
@@ -0,0 +1,12 @@
+package com.lara.StringPractice;
+
+public class Test1
+{
+	public static void main(String[] args)
+	{
+		String name = "Sailendra Narayan Jena";
+		char test = name.charAt(5);
+		
+		System.out.println(test);
+	}
+}
