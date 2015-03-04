18a21d3b0ebc15746b0677f7530d14e184624938
sailendra.n.jena@gmail.com <sailendra.n.jena@gmail.com@376d9f85-88c3-4b5c-aff6-862f136f3076>
2015-03-04 10:33:34 +0000

    Changes Done By Sailendra Narayan Jena

--- trunk/Utility/src/com/AccessDatabase/Test/Test.java
+++ trunk/Utility/src/com/AccessDatabase/Test/Test.java
@@ -17,7 +17,9 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
+import com.myAssets.utils.GmailUtil;
 import com.myAssets.utils.LogFileUtil;
+import com.myAssets.utils.PropOutUtil;
 import com.myAssets.utils.PropUtil;
 
 
@@ -27,76 +29,87 @@ import com.myAssets.utils.PropUtil;
  */
 public class Test
 {
+//	private static Message message = null;
+//	private static BodyPart messageBodyPart = null;
+//	private static final DataSource source = null;
+//	private static Session session = null;
+//	private static Multipart multipart = null;
+//	
+//	
+//	private static final String username = PropOutUtil.getValues("username");
+//	private static final String password = PropOutUtil.getValues("password");
+//	private static final String subjectLine = PropOutUtil.getValues("subjectLine");
+//	private static final String contentLine = PropOutUtil.getValues("contentLine");
+//	private static final String filename = PropOutUtil.getValues("attachedFilePath");
+			
+			
 	public static void main(String[] args) 
 	{
-		String to = PropUtil.getValues("username");
+//		String to = PropUtil.getValues("username");
+//		
+//		String from = PropUtil.getValues("username");
 		
-		String from = PropUtil.getValues("username");
-		
-	      final String username = "sailendra.n.jena@gmail.com";//change accordingly
-	      final String password = PropUtil.getValues("password");//change accordingly
+//	      final String username = "sailendra.n.jena@gmail.com";//change accordingly
+//	      final String password = PropUtil.getValues("password");//change accordingly
 
 	      // Assuming you are sending email through relay.jangosmtp.net
-	      String host = "smtp.gmail.com";
-
-	      Properties props = new Properties();
-	      props.put("mail.smtp.auth", "true");
-	      props.put("mail.smtp.starttls.enable", "true");
-	      props.put("mail.smtp.host", host);
-	      props.put("mail.smtp.port", "587");
-
-	      // Get the Session object.
-	      Session session = Session.getInstance(props,
-	         new javax.mail.Authenticator() {
-	            protected PasswordAuthentication getPasswordAuthentication() {
-	               return new PasswordAuthentication(username, password);
-	            }
-	         });
-
-	      try {
-	         // Create a default MimeMessage object.
-	         Message message = new MimeMessage(session);
-
-	         // Set From: header field of the header.
-	         message.setFrom(new InternetAddress(from));
-
-	         // Set To: header field of the header.
-	         message.setRecipients(Message.RecipientType.TO,
-	            InternetAddress.parse(to));
-
-	         // Set Subject: header field
-	         message.setSubject("Testing Subject");
-
-	         // Create the message part
-	         BodyPart messageBodyPart = new MimeBodyPart();
-
-	         // Now set the actual message
-	         messageBodyPart.setText("This is message body");
-
-	         // Create a multipar message
-	         Multipart multipart = new MimeMultipart();
-
-	         // Set text message part
-	         multipart.addBodyPart(messageBodyPart);
-
-	         // Part two is attachment
-	         messageBodyPart = new MimeBodyPart();
-	         String filename = "D:\\test.txt";
-	         DataSource source = new FileDataSource(filename);
-	         messageBodyPart.setDataHandler(new DataHandler(source));
-	         messageBodyPart.setFileName(filename);
-	         multipart.addBodyPart(messageBodyPart);
-
-	         // Send the complete message parts
-	         message.setContent(multipart);
-
-	         // Send message
-	         Transport.send(message);
-
-	         System.out.println("Sent message successfully....");
-	  
-	      } catch (MessagingException e) {
-	         throw new RuntimeException(e);
-	      }
+//	      String host = "smtp.gmail.com";
+//
+//	      Properties props = new Properties();
+//	      props.put("mail.smtp.auth", "true");
+//	      props.put("mail.smtp.starttls.enable", "true");
+//	      props.put("mail.smtp.host", host);
+//	      props.put("mail.smtp.port", "587");
+//
+//	      // Get the Session object.
+//	      session = GmailUtil.getSession(props, username, password);
+//
+//	      try {
+//	         // Create a default MimeMessage object.
+//	         message = new MimeMessage(session);
+//
+//	         // Set From: header field of the header.
+//	         message.setFrom(new InternetAddress(username));
+//
+//	         // Set To: header field of the header.
+//	         message.setRecipients(Message.RecipientType.TO,
+//	            InternetAddress.parse(username));
+//
+//	         // Set Subject: header field
+//	         message.setSubject(subjectLine);
+//
+//	         // Create the message part
+//	         messageBodyPart = new MimeBodyPart();
+//
+//	         // Now set the actual message
+//	         messageBodyPart.setText(contentLine);
+//
+//	         // Create a multipar message
+//	         multipart = new MimeMultipart();
+//
+//	         // Set text message part
+//	         multipart.addBodyPart(messageBodyPart);
+//
+//	         // Part two is attachment
+//	         messageBodyPart = new MimeBodyPart();
+////	         String filename = "D:\\test.txt";
+//	         DataSource source = new FileDataSource(filename);
+//	         messageBodyPart.setDataHandler(new DataHandler(source));
+//	         messageBodyPart.setFileName(filename);
+//	         multipart.addBodyPart(messageBodyPart);
+//
+//	         // Send the complete message parts
+//	         message.setContent(multipart);
+//
+//	         // Send message
+//	         Transport.send(message);
+//
+//	         System.out.println("Sent message successfully....");
+//	  
+//	      } catch (MessagingException e) {
+//	         throw new RuntimeException(e);
+//	      }
+		
+		GmailUtil.attachOneFileMail();
 	}
 }
