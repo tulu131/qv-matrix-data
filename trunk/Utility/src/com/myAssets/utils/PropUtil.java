7c5046c0a5b7f93cb3db77404b3744086085990f
sailendra.n.jena@gmail.com <sailendra.n.jena@gmail.com@376d9f85-88c3-4b5c-aff6-862f136f3076>
2015-03-11 12:15:59 +0000

    Changes Done By Sailendra Narayan Jena

--- trunk/Utility/src/com/myAssets/utils/PropUtil.java
+++ trunk/Utility/src/com/myAssets/utils/PropUtil.java
@@ -13,11 +13,12 @@ import java.util.Properties;
  */
 public class PropUtil
 {
+
 	public static File file = null;
 	public static FileInputStream fileInput = null;
 	public static Properties props = null;
 
-
+	
 	static
 	{
 		file = new File("src\\com\\myAssets\\resources\\config.properties");
@@ -38,11 +39,43 @@ public class PropUtil
 		}
 
 	}
-	
 	public static String getValues(String key)
 	{
-		String value = props.getProperty(key);
+		Properties props = new Properties();
+		String value = props.getProperty("smtpAuthValues");
 		
 		return value;
+	}
+
+	
+
+	//SMTP Details
+	private static final String smtpAuthValues = getValues("smtpAuthValues");
+	private static final String smtpStarttlsValues = getValues("smtpStarttlsValues");
+	private static final String smtpHostValues = getValues("smtpHostValues");
+	private static final String smtpPortValues = getValues("smtpPortValues");
+	
+	private static final String smtpAuthKey = getValues( "smtpAuthKey");
+	private static final String smtpStarttlsKey = getValues("smtpStarttlsKey");
+	private static final String smtpHostKey = getValues("smtpHostKey");
+	private static final String smtpPortKey = getValues("smtpPortKey");
+
+
+	
+	
+	public static Properties getSMTPProps()
+	{
+		props = System.getProperties();
+		props.put(smtpAuthKey, smtpAuthValues);
+		props.put(smtpStarttlsKey, smtpStarttlsValues);
+		props.put(smtpHostKey, smtpHostValues);
+		props.put(smtpPortKey, smtpPortValues);
+		
+		return props;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(smtpHostKey);
 	}
 }
