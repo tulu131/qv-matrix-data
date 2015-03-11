f8c5d4c5a447ff06af02a2ebbc29e48d5e08416c
sailendra.n.jena@gmail.com <sailendra.n.jena@gmail.com@376d9f85-88c3-4b5c-aff6-862f136f3076>
2015-03-11 12:16:36 +0000

    Changes Done By Sailendra Narayan Jena

--- trunk/Utility/src/com/AccessDatabase/Test/Test.java
+++ trunk/Utility/src/com/AccessDatabase/Test/Test.java
@@ -3,20 +3,18 @@ package com.AccessDatabase.Test;
 import com.myAssets.utils.GmailUtil;
 import com.myAssets.utils.PropOutUtil;
 
+import java.util.List;
 import java.util.Properties;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Multipart;
 import javax.mail.SendFailedException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 
-import com.myAssets.utils.GmailUtil;
-import com.myAssets.utils.PropOutUtil;
+import com.myAssets.utils.FileUtil;
 import com.myAssets.utils.PropUtil;
 
 
@@ -46,18 +44,18 @@ public class Test
 			
 			String mailFrom = PropUtil.getValues("username");
 			String mailTo = PropUtil.getValues("username");
-			String[] mailAddressTo = {"sailendra.narayan.jena@gmail.com", "sailendra.jena@niit-tech.com"};
+			List<String> mailAddressTo = FileUtil.getMailLists();
 			message.setFrom(new InternetAddress(mailFrom));
-			InternetAddress[] mailAddress_To = new InternetAddress[mailAddressTo.length];
+			InternetAddress[] mailAddress_To = new InternetAddress[mailAddressTo.size()];
 			
-			for (int i = 0; i < mailAddressTo.length; i++)
+			for (int i = 0; i < mailAddressTo.size(); i++)
 			{
-				mailAddress_To[i] = new InternetAddress(mailAddressTo[i]);
+				mailAddress_To[i] = new InternetAddress(mailAddressTo.get(i));
 			}
 			message.addRecipients(Message.RecipientType.TO, mailAddress_To);
 			String subject = PropOutUtil.getValues("subjectLine");
 			message.setSubject(subject);
-			message.setContent(messageBody, "text/html");
+			message.setText(messageBody);
 			Transport.send(message);
 			status = true;
 		}
@@ -74,7 +72,6 @@ public class Test
 	
 	public static void main(String[] args) 
 	{
-		GmailUtil.attachOneFileMail();
 		String messageBody = PropOutUtil.getValues("contentLine");
 		boolean status = send(messageBody);
 		
