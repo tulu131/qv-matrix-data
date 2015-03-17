aad8951cc696097909a9894c8a1ac00a00aea155
sailendra.n.jena@gmail.com <sailendra.n.jena@gmail.com@376d9f85-88c3-4b5c-aff6-862f136f3076>
2015-03-17 05:39:06 +0000

    Changes Done By Sailendra Narayan Jena

--- trunk/Utility/src/com/test/TestSample.java
+++ trunk/Utility/src/com/test/TestSample.java
@@ -3,7 +3,12 @@ package com.test;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Properties;
+import java.util.Scanner;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -61,7 +66,7 @@ class Tested
 }
 public class TestSample
 {
-	public static void main(String[] args) throws JSONException
+	public static void main(String[] args) throws JSONException, IOException
 	{
 //		Properties props = new Properties();
 //		InputStream input = null;
@@ -101,10 +106,35 @@ public class TestSample
 //		System.out.println(username);
 		
 		
-		String url = "https://sandbox.qa.virginamerica.com/api/v0/booking/lock-inventory-cabin/Post";
-		JSONParser json = new JSONParser();
-		JSONObject object = json.getJSONFromUrl(url);
-		String content = object.getString("Source");
+//		String url = "https://sandbox.qa.virginamerica.com/api/v0/booking/lock-inventory-cabin/Post";
+//		JSONParser json = new JSONParser();
+//		JSONObject object = json.getJSONFromUrl(url);
+//		String content = object.getString("Source");
+		String addr = "Post";
+		String s = "http://maps.google.com/maps/api/geocode/json?" +"sensor=false&address=";
+		s += URLEncoder.encode(addr, "UTF-8");
+		URL url = new URL(s);
+
+		// read from the URL
+		Scanner scan = new Scanner(url.openStream());
+		String str = new String();
+		while (scan.hasNext())
+		    str += scan.nextLine();
+		scan.close();
+		
+		// build a JSON object
+		JSONObject obj = new JSONObject(str);
+		if (! obj.getString("status").equals("OK"))
+		    return;
+		
+		// get the first result
+		JSONObject res = obj.getJSONArray("results").getJSONObject(0);
+		System.out.println(res.getString("formatted_address"));
+		JSONObject loc =
+		    res.getJSONObject("geometry").getJSONObject("location");
+		System.out.println("lat: " + loc.getDouble("lat") +
+                    ", lng: " + loc.getDouble("lng"));
+		
 	}
 		
 
