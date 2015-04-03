2c51b4ca19aeaeb93eaa8578f06b2e69353075dd
sailendra.n.jena@gmail.com <sailendra.n.jena@gmail.com@376d9f85-88c3-4b5c-aff6-862f136f3076>
2015-04-03 10:53:02 +0000

    Added file By Sailendra Narayan Jena

---
+++ SpringExamples/src/com/lara/beanClasses/PresentAddress.java
@@ -0,0 +1,69 @@
+package com.lara.beanClasses;
+
+public class PresentAddress
+{
+	private String houseNo;
+	private String streetName;
+	private String cityName;
+	private String landMark;
+	private String stateName;
+	private String countryName;
+	private long pinNo;
+
+	public String getHouseNo()
+	{
+		return houseNo;
+	}
+	public void setHouseNo(String houseNo)
+	{
+		this.houseNo = houseNo;
+	}
+	public String getStreetName()
+	{
+		return streetName;
+	}
+	public void setStreetName(String streetName)
+	{
+		this.streetName = streetName;
+	}
+	public String getCityName()
+	{
+		return cityName;
+	}
+	public void setCityName(String cityName)
+	{
+		this.cityName = cityName;
+	}
+	public String getLandMark()
+	{
+		return landMark;
+	}
+	public void setLandMark(String landMark)
+	{
+		this.landMark = landMark;
+	}
+	public String getStateName()
+	{
+		return stateName;
+	}
+	public void setStateName(String stateName)
+	{
+		this.stateName = stateName;
+	}
+	public String getCountryName()
+	{
+		return countryName;
+	}
+	public void setCountryName(String countryName)
+	{
+		this.countryName = countryName;
+	}
+	public long getPinNo()
+	{
+		return pinNo;
+	}
+	public void setPinNo(long pinNo)
+	{
+		this.pinNo = pinNo;
+	}
+}
