066bcbc6034002f0b7a7984b31a762ad0912a79e
sailendra.n.jena@gmail.com <sailendra.n.jena@gmail.com@376d9f85-88c3-4b5c-aff6-862f136f3076>
2015-05-11 11:31:57 +0000

    Changes Done By Sailendra Narayan Jena

--- StingPractice/src/com/lara/test/Test.java
+++ StingPractice/src/com/lara/test/Test.java
@@ -1,15 +1,88 @@
 package com.lara.test;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.List;
+
+import sun.org.mozilla.javascript.internal.regexp.SubString;
+
+
+
 public class Test
 {
-	public static void main(String[] args)
-	{
-		String TRUE_CLIENT_IP = "True-Client-IP";
-		String[] name = TRUE_CLIENT_IP.split("-");
-		
-		for (int i = 0; i < name.length; i++)
-		{
-			System.out.println(name[i]);
-		}
-	}
+	 
+	  public static void main(String[] args)
+	  {
+	 
+		  Test fileSearch = new Test();
+	 
+		  fileSearch.searchDirectory(new File("D:\\Dump Folders\\2015 Dump\\May\\11May2015\\OWDir1A1CMCRIntlGoldTBVcoTest\\backup\\BookingController.lockInventory"));
+	 
+	  }
+	  public void searchDirectory(File directory)
+	  {
+	 
+			if (directory.isDirectory())
+			{
+			    search(directory);
+			}
+			else
+			{
+			    System.out.println(directory.getAbsoluteFile() + " is not a directory!");
+			}
+	 
+	  }
+	 
+	  private void search(File file)
+	  {
+	 
+			if (file.isDirectory())
+			{
+					String folderName = file.getName();
+					String newFName = null;
+					if (folderName.contains("_"))
+					{
+						newFName = folderName;
+						String newFolder = newFName.substring(0, newFName.indexOf("_"));
+						
+						System.out.println(newFolder);
+						System.out.println(file.getParent());
+						File newDir = new File(file.getParent()+"\\"+newFolder);
+						if (file.isDirectory())
+						{
+							file.renameTo(newDir);
+							System.out.println("Old Directory got renamed successfully");
+						}
+						else
+						{
+							file.mkdir();
+							file.renameTo(newDir);
+							System.out.println("new Directory Done");
+						}
+					}
+					
+			            //do you have permission to read this directory?	
+				    if (file.canRead())
+				    {
+						for (File temp : file.listFiles())
+						{
+							    if (temp.isDirectory())
+							    {
+								search(temp);
+							    }
+							    else
+							    {
+							    	System.out.println("Directory already renamed");
+							    	System.exit(0);
+							    }
+						}
+			 
+				    }
+				    else
+				    {
+				    	System.out.println(file.getAbsoluteFile() + "Permission Denied");
+				    }
+		      }
+	  }//End of seach() method.
 }
